f5681505773c503415936e75e17c089091552eac
mfmstr <g.kharebava01@gmail.com>
2023-03-08 15:45:43 +0400

    Adding MyRunnable class which implements Runnable Interface

--- multithreading/src/multithreading/Main.java
+++ multithreading/src/multithreading/Main.java
@@ -1,5 +1,16 @@
 package multithreading;
 
 public class Main {
-
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Thread.currentThread().getName());
+		
+		MyRunnable myRunnable = new MyRunnable();
+		Thread th = new Thread(myRunnable);
+		
+		th.start();
+		
+	}
+	
 }
---
+++ multithreading/src/multithreading/MyRunnable.java
@@ -0,0 +1,12 @@
+package multithreading;
+
+public class MyRunnable implements Runnable {
+
+	@Override
+	public void run() {
+		
+		System.out.println("Hello, from another thread: " + Thread.currentThread().getName());
+		
+	}
+
+}
